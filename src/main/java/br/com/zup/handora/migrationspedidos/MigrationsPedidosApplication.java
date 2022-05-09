9cbe16b7ee876c1b5394dad4a2d5f225a982d52c
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-09 16:40:23 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/migrationspedidos/MigrationsPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.migrationspedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationsPedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/migrationspedidos/MigrationsPedidosApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.migrationspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
