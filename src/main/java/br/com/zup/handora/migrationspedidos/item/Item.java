3fc6f7ac599bba86580ca8d407801db5909c9481
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-10 00:21:04 -0300

    Add id attr to item entity
    Using the SKU as an id is wrong, because you won't be able to add the
    same product to two or more orders (same SKU, different items).

--- src/main/java/br/com/zup/handora/migrationspedidos/item/Item.java
+++ src/main/java/br/com/zup/handora/migrationspedidos/item/Item.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -15,6 +17,10 @@ import br.com.zup.handora.migrationspedidos.pedido.Pedido;
 public class Item {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String sku;
 
     @Column(nullable = false)
